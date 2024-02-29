9fecbe42c042205036f83232f8788285374273af
Amanda Robert <irizrob898@gmail.com>
2024-02-29 07:40:12 -0800

    first commit of test template

---
+++ GUI/test_df_gui.py
@@ -0,0 +1,18 @@
+import unittest
+import df_gui_YK
+import pandas as pd
+import numpy as np
+import math 
+
+class UnitTestsGui(unittest.TestCase):
+    def test_upload_file(self):
+
+    def test_visualize_data(self):
+
+    def test_generate_plot(self):
+
+    def test_custom_plot(self):
+
+    def test_main(self):
+
+
