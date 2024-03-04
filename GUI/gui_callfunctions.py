368064d0aa5e8cd59f6f75de2949e0937fcac381
Amanda Robert <irizrob898@gmail.com>
2024-03-03 17:21:16 -0800

    fixed "downloading output csv" and partially fixed dataframe replacement added a few tests. difficult to replicate streamlit environment

--- GUI/gui_callfunctions.py
+++ GUI/gui_callfunctions.py
@@ -18,6 +18,10 @@ def upload_model():
     uploaded_file = st.file_uploader('Upload file')
 
     if uploaded_file is not None:
+        # Check if the uploaded file is a .csv file
+        if uploaded_file.type not in ['text/csv', 'text/plain']:
+            st.error("Invalid format, please upload a .txt or .csv file")
+            return
         # Display the file name if file is uploaded
         file_name = uploaded_file.name
         st.write("Your uploaded file name:", file_name)
@@ -30,53 +34,63 @@ def upload_model():
         st.write(model_load)
 
         st.session_state.uploaded_file = uploaded_file
+        st.session_state.selected_option = selected_option
         st.session_state.model_load = model_load
     else:
         st.write("Please upload a file")
 
 def solve_model():
-    st.header("fill out info below to solve your model in tellurium")
+    st.header("Tellurium/Roadrunner Modeling")
+    st.subheader("Fill out information below to solve your model using tellurium")
 
-    #inputs
-    t0 = st.number_input("Enter your t0", value=0)
-    t1 = st.number_input("Enter your t1", value=0)
+    # Time inputs
+    t0 = st.number_input("Enter your starting time: t0", value=0)
+    tf = st.number_input("Enter your final time: tf", value=0)
     steps = st.number_input("Enter the number of steps you want", value=0)
 
     # Button to trigger processing
     if st.button("Solve Model"):
         if "model_load" in st.session_state:
             model_load = st.session_state.model_load
+            #selected_option = st.session_state.selected_option
             try:
-                df = simulate_model(model_load, t0, t1, steps)
+                #file_content = uploaded_file.read()
+                #model_load = load_model(file_content, selected_option)
+                df = simulate_model(model_load, t0, tf, steps)
                 # returns result, df, species_names
+                if "df" in st.session_state:
+                    old_df = st.session_state.df
+                    df = pd.concat([old_df, df], ignore_index=True)
+                    
                 st.write(df.head())
                 st.session_state.df = df
             except Exception as e:
                 st.error(f"An error occurred: {e}")
         else:
-            st.write("load model first")
+            st.write("Load model first")
     
 
     # Button to download the DataFrame as a CSV file
     if st.button("Download CSV"):
         # Create a link to download the CSV file
         if "df" in st.session_state:
-            df = st.session_state.df
             try:
+                df = st.session_state.df
                 csv = df.to_csv(index=False)
                 b64 = base64.b64encode(csv.encode()).decode()  # Convert DataFrame to bytes
-                href = f'<a href="solved_model:file/csv;base64,{b64}" download="solved_model.csv">Download CSV File</a>'
+                href = f'<a href="data:file/csv;base64,{b64}" download="solved_model.csv">Download CSV File</a>'
+                # ="solved_model:file/csv;base64,{b64}" download="solved_model.csv">Download CSV # File</a>'
                 st.markdown(href, unsafe_allow_html=True)
             except Exception as e:
                 st.error(f"An error occurred: {e}")
         else:
-            st.write("load model first")
+            st.write("Load model first")
 
 
 ##### PLOTTING!!!!!!!!!!!!!!!!!!!
-# Function to generate plot
+# Function to generate and display plot
 def visualize_data():
-    st.header("All data")
+    st.header("Plotting All Data")
     if "df" in st.session_state:
         df = st.session_state.df
         try:
@@ -85,7 +99,8 @@ def visualize_data():
         except Exception as e:
             st.error(f"An error occurred: {e}")
     else:
-        st.write("load model first")
+        st.write("Load model first")
+
 # Function to generate plot with selected columns
 def generate_plot(df, x_column, y_columns):
     fig = go.Figure()
@@ -93,26 +108,44 @@ def generate_plot(df, x_column, y_columns):
         fig.add_trace(go.Scatter(x=df[x_column], y=df[y_column], mode='lines', name=y_column))
     fig.update_layout(title='Customizable Plot', xaxis_title=x_column, yaxis_title='Value')
     return fig
+
 def custom_plot():
     st.header("Customizable Plot")
     if "df" in st.session_state:
         df = st.session_state.df
-        # Display available columns and checkboxes
-        st.write('Select Columns:')
-        columns = df.columns.tolist()
-        selected_columns = [st.checkbox(col, value=False) for col in columns]
+        # Display dropdown menu to choose X-axis 
+        st.subheader("Select x-axis data:")
+        x_column = st.selectbox('x-axis:', options=df.columns.tolist())
+
+        # Display dropdown menu to choose Y-axis 
+        st.subheader("Select Y axis data:")
+        # User can select multiple species to plot
+        y_columns = st.multiselect('Y axis:', options=df.columns.tolist())
+
+        # Display input fields for user customization
+        st.subheader("Plot Customization")
+        title = st.text_input("Title", "Custom Plot")
+        x_label = st.text_input("X-Axis Label", "X")
+        y_label = st.text_input("Y-Axis Label", "Y")
+        
+        #st.write('Select Columns:')
+        #columns = df.columns.tolist()
+        #selected_columns = [st.checkbox(col, value=False) for col in columns]
+        
         # Generate plot
-        x_column = st.selectbox('Select X axis data:', options=columns)
-        y_columns = [col for col, selected in zip(columns, selected_columns) if selected and col != x_column]
+        #x_column = st.selectbox('Select X axis data:', options=columns)
+        #y_columns = [col for col, selected in zip(columns, selected_columns) if selected and col #!= x_column]
+        # Generate and display the plot      
         if y_columns:
             fig = generate_plot(df, x_column, y_columns)
+            fig.update_layout(title=title, xaxis_title=x_label, yaxis_title=y_label)
             st.plotly_chart(fig)
         else:
             st.write('No Y columns selected.')
         # Print selected columns
         #st.write('Selected Y Columns:', [col for col, selected in zip(columns, selected_columns) if selected and col != x_column])
     else:
-        st.write("load model first")
+        st.write("Load model first")
 
 
 
