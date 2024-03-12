733da5fa82190a7a0bc98a75fae38e8571f73ecc
Karl Anderson <kba6@uw.edu>
2024-03-11 19:38:00 -0700

    adding test for best_FRENDAs.py

---
+++ GUI/test_best_FRENDAs.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+
+# Import the function you want to test
+from best_FRENDAs import *
+
+'''To do - unittests for all functions in best_FRENDAs'''
+
